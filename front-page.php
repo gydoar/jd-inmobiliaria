1ce17bc132a69bfe05a5ff8f524dbc77f69092ad
Andres Vega <gydoar@hotmail.com>
2015-03-25 13:30:46 -0500

    Iniciando el commit

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+	<section id="banner">
+		<div id="copi">
+			<p>"ADMINISTRAMOS <strong>SU INMUEBLE</strong><br>CON SEGURIDAD PARA QUE <br><strong>VIVA CON TRAQUILIDAD"</strong></p>
+		</div>
+		<div id="form">
+			<label for=""></label>
+			 <input type="radio" name="inmueble" value="venta" /> Venta<br />
+             <input type="radio" name="inmueble" value="arriendo" /> Arriendo<br />
+             <input type="radio" name="inmueble" value="todos" /> Todos<br />
+             <input type="submit" value="Buscar" />
+		</div>
+	</section>
+
+	<section class="container" id="body">
+		<div class="twelve columns" id="garantia">
+			<h2>GARANTIA</h2>
+			<p><strong>JDAVID BELTRAN INMOBILIARIA SAS</strong> dentro de su seriedad y responsabilidad social garantiza.</p>
+		</div>
+	</section>
+
+<?php get_footer(); ?>
