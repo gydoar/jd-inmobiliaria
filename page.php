bfd98fd60c87e46f9a4f503b2c06a9616a7dacde
Andres Vega <gydoar@hotmail.com>
2015-04-10 10:57:42 -0500

    agregando página quienes somos

--- page.php
+++ page.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
-	<article id="page">
+	<article class="container" id="page">
+
+		<div class="ten columns offset-by-one">
 		
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
 
-			<?php the_content(); ?>
+			<?php endwhile; else : ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
 
-		<?php endwhile; else : ?>
-			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-		<?php endif; ?>
+		</div>
 
 	</article>
 
