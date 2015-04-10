a6af3279977f05eb249b33f8e6654adb7a9a840d
Andres Vega <gydoar@hotmail.com>
2015-04-10 12:19:22 -0500

    terminando pagina servicios

---
+++ page-servicios.php
@@ -0,0 +1,74 @@
+<?php get_header(); ?>
+	<div class="container" id="page--ext">
+
+		<h2>SERVICIOS</h2>
+		<div class="row">
+			<div class="six columns">
+				<img src="<?php bloginfo('template_url' ); ?>/library/img/icon-ventas.png" alt="">&nbsp;&nbsp;<h3>VENTAS</h3>
+					<div class="offset-by-one eleven columns">
+						<p>Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. </p>
+
+						<p>persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos quedando esencialmente.</p>
+					</div>
+					<div class="row center">
+						<br>
+						<a class="btn--red" href="#">Ver Propiedades</a>
+					</div>
+					<br><br>
+					<!-- Sector 2 -->
+				<img src="<?php bloginfo('template_url' ); ?>/library/img/icon-arriendo.png" alt="">&nbsp;&nbsp;<h3>ARRENDAMIENTOS</h3>
+					<div class="offset-by-one eleven columns">
+						<p>Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. </p>
+
+						<p>No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos quedando esencialmente igual.</p>
+
+						<ul class="list__arri">
+							<li>No sólo sobrevivió 500 años, sino que tambien.</li>
+							<li>No sólo sobrevivió 500 años, sino que tambien.</li>
+							<li>No sólo sobrevivió 500 años, sino que tambien.</li>
+
+						</ul>
+					</div>
+					<div class="row center">
+						<a class="btn--red" href="#">Ver Propiedades</a>
+					</div>
+					
+			</div>
+			<!-- columna 2 -->
+			<div class="six columns">
+				<img src="<?php bloginfo('template_url' ); ?>/library/img/icon-inmueble.png" alt="">&nbsp;&nbsp;<h3>ADMINISTRACIÓN DE INMUEBLES</h3>
+					<div class="offset-by-one eleven columns">
+						<p>Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. </p>
+
+						<p>No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos quedando esencialmente.</p>
+
+					</div>
+					<div class="row center">
+						<a class="btn--red" href="#">Más Información</a>
+					</div>
+					<br><br>
+				<!--sector 2 -->
+				<img src="<?php bloginfo('template_url' ); ?>/library/img/icon-avaluos.png" alt="">&nbsp;&nbsp;<h3>AVALÚOS</h3>
+					<div class="offset-by-one eleven columns">
+						<p>Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido.</p>
+
+					</div>
+					<div class="row center">
+						<a class="btn--red" href="#">Más Información</a>
+					</div>
+
+				<br><br><br>
+				<!--sector 3 -->
+				<img src="<?php bloginfo('template_url' ); ?>/library/img/icon-mantenimiento.png" alt="">&nbsp;&nbsp;<h3>MANTENIMIENTO Y REMODELACIÓN DE INMUEBLES</h3>
+					<div class="offset-by-one eleven columns">
+						<p>Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
+					<br>
+					</div>
+					<div class="row center">
+						<a class="btn--red" href="#">Más Información</a>
+					</div>
+			</div>
+		</div>
+		
+	</div>
+<?php get_footer(); ?>
